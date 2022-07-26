3d4586022fef44df46c67d8e14759e5cad317f88
nousr <51308183+nousr@users.noreply.github.com>
2022-07-25 20:54:14 -0700

    block in setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dream_bench",
+    packages=find_packages(exclude=[]),
+    version="0.0.1",
+    author="Zion English",
+    description="A simple package for benchmarking text to image models.",
+    keywords=["text2img", "benchmark"],
+    install_requires=[],
+)
