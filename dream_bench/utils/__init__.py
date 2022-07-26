26e6d594f99d283dad72242405c97179e9fc32aa
nousr <51308183+nousr@users.noreply.github.com>
2022-07-25 20:52:38 -0700

    seperate concerns of utility functions & add tokenizer

---
+++ dream_bench/utils/__init__.py
@@ -0,0 +1,2 @@
+from .helpers import exists, filename_from_path, import_or_print_error, is_url
+from .tokenizer import tokenizer
---
+++ dream_bench/utils/helpers.py
@@ -0,0 +1,26 @@
+from click import secho
+from importlib import import_module
+
+
+def is_url(x: str):
+    if x.startswith("http://") or x.startswith("https://"):
+        return True
+
+    return False
+
+
+def exists(x):
+    return x is not None
+
+
+def filename_from_path(path):
+    return path.split("/")[-1]
+
+
+def import_or_print_error(pkg_name, err_str=None, **kwargs):
+    try:
+        return import_module(pkg_name)
+    except ModuleNotFoundError as _:
+        if exists(err_str):
+            secho(err_str, **kwargs)
+        exit()
