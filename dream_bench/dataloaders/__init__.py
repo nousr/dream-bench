e7224f36ac135fd0d0964e28cde579a85a5f923b
zion <51308183+nousr@users.noreply.github.com>
2022-07-27 17:35:50 -0700

    remove dataloader module

--- dream_bench/dataloaders/__init__.py
+++
@@ -1,1 +0,0 @@
-from .benchmark_dataset import BenchmarkDataset
--- dream_bench/dataloaders/benchmark_dataset.py
+++
@@ -1,7 +0,0 @@
-class BenchmarkDataset():
-    """
-    A wrapper for webdataset that handles returning the appropriate data
-    """
-
-    def __init__(self) -> None:
-        pass
