45cfdc1a77de78885fbe4b012379e2a8b21ca61c
nousr <51308183+nousr@users.noreply.github.com>
2022-08-02 15:17:24 -0700

    make dataset generation more efficient

--- prompts2datset.py
+++ prompts2datset.py
@@ -3,24 +3,59 @@
 import os
 import json
 import click
+import torch
+import numpy as np
 from torch.nn import Module
 from torch.cuda import is_available
 from webdataset import ShardWriter
 from dream_bench.load_models import load_clip, load_prior, filename_from_path
 from dream_bench.tokenizer import tokenizer
+from dream_bench.helpers import print_ribbon
 
 DEVICE = "cuda" if is_available() else "cpu"
 TOKENIZER_CONTEXT_LENGTH = 77
 TOKENIZER_TRUNCATE_TEXT = True
 
 
+def txt2embeddings(
+    model: Module, architecture: str, text: torch.Tensor, batch_size: int
+):
+    """
+    Predict embeddings for captions using a given batch size.
+
+    Returns:
+        - All predicted embeddings batched along the first dimension and converted to numpy format.
+    """
+    click.secho(print_ribbon(f"Creating Embeddings With: {architecture}"), fg="green")
+
+    embeds = list()
+
+    for i in range(0, len(text), batch_size):
+        model_input = text[i : i + batch_size]
+
+        if architecture == "prior":
+            embeds.append(model.sample(model_input).detach().cpu())
+        elif architecture == "clip":
+            embeds.append(model.encode_text(model_input).detach().cpu())
+        else:
+            click.secho(
+                f"The {architecture} architecture is not yet supported.", fg="red"
+            )
+            exit(1)
+
+    return torch.cat(embeds, dim=0).contiguous().numpy()
+
+
 def wds_create(
     prompts: list,
+    batch_size: int,
     output_folder: str,
     filename: str,
     clip_model: Module,
     prior_model: Module,
-    options: dict,
+    predict_image: bool,
+    embed_text: bool,
+    tokenize_text: bool,
 ):
     """
     Create a webdataset for the prompt list.
@@ -28,85 +63,126 @@ def wds_create(
     # TODO: docstring
     """
     filename = filename.split(".")[0]
+
+    # precompute embeddings for all requested formats
+    tokenized_text = (
+        tokenizer.tokenize(
+            prompts,
+            context_length=TOKENIZER_CONTEXT_LENGTH,
+            truncate_text=TOKENIZER_TRUNCATE_TEXT,
+        )
+        .to(DEVICE)
+    )
+    clip_text_embeddings = (
+        txt2embeddings(
+            model=clip_model,
+            text=tokenized_text,
+            batch_size=batch_size,
+            architecture="clip",
+        )
+        if embed_text
+        else None
+    )
+    prior_image_embeddings = (
+        txt2embeddings(
+            model=prior_model,
+            text=tokenized_text,
+            batch_size=batch_size,
+            architecture="prior",
+        )
+        if predict_image
+        else None
+    )
+
     with ShardWriter(pattern=f"{output_folder}/{filename}-%04d.tar") as sink:
-        for idx, prompt in enumerate(prompts):
-            tar_keys = {"__key__": "%04d" % idx}
+        for idx, caption in enumerate(prompts):
+            tar_keys = {"__key__": "%04d" % idx, "caption.txt": caption}
 
-            # always save original caption as string & tokenize for simplicity
-
-            tar_keys["caption.txt"] = prompt
-            tokenized_prompt = tokenizer.tokenize(
-                prompt,
-                context_length=TOKENIZER_CONTEXT_LENGTH,
-                truncate_text=TOKENIZER_TRUNCATE_TEXT,
-            ).to(DEVICE)
-
-            for option, in_use in options.items():
-                if not in_use:
-                    continue
-
-                if option == "tokenized_text":
-                    tar_keys[option + ".npy"] = tokenized_prompt.detach().cpu().numpy()
-
-                elif option == "clip_text_embed":
-                    tar_keys[option + ".npy"] = (
-                        clip_model.encode_text(tokenized_prompt).detach().cpu().numpy()
-                    )
-
-                elif option == "prior_image_embed":
-                    tar_keys[option + ".npy"] = (
-                        prior_model.sample(tokenized_prompt).detach().cpu().numpy()
-                    )
+            if predict_image:
+                tar_keys["prior_image_embedding.npy"] = prior_image_embeddings[idx]
+            if embed_text:
+                tar_keys["clip_text_embedding.npy"] = clip_text_embeddings[idx]
+            if tokenize_text:
+                tar_keys["tokenized_text.npy"] = tokenized_text[idx].detach().cpu().numpy()
 
             sink.write(tar_keys)
 
 
 @click.command()
 @click.option(
-    "--prompt-list", help="A json file containing a list of prompts.", required=True
+    "--prompt-list",
+    type=str,
+    help="A json file containing a list of prompts.",
+    required=True,
 )
 @click.option(
-    "--output-folder", help="parent folder of the dataset to be created", required=True
+    "--output-folder",
+    type=str,
+    help="parent folder of the dataset to be created",
+    required=True,
 )
 @click.option(
-    "--override-folder",
-    default=False,
-    help="wether to overrite the output folder if it exists",
+    "--batch-size",
+    default=512,
+    type=int,
+    help="Number of prompts to process in parallel.",
+)
+@click.option(
+    "--prompt-repeat",
+    default=1,
+    type=int,
+    help="Number of times to run inference for a prompt.",
 )
 @click.option(
     "--embed-text",
-    default=False,
+    is_flag=True,
     help="Wether to use clip to embed the prompts as text embeddings",
 )
-@click.option("--tokenize-text", default=True, help="Wether to tokenize the text.")
+@click.option(
+    "--tokenize-text",
+    is_flag=True,
+    help="Wether to save the tokenized the text.",
+)
 @click.option(
     "--predict-image",
-    default=True,
+    is_flag=True,
     help="Wether to use a diffusion prior to create an image embedding from each prompt.",
 )
 @click.option(
-    "--clip-model", default=None, help="Clip model to use for embedding images."
+    "--clip-model",
+    default=None,
+    type=str,
+    help="Clip model to use for embedding images.",
 )
 @click.option(
     "--prior-checkpoint",
     default=None,
+    type=str,
     help="A local path or web url to a prior config file.",
 )
 @click.option(
     "--prior-config",
     default=None,
+    type=str,
     help="A local path or web url to a prior checkpoint.",
+)
+@click.option(
+    "--override-folder",
+    is_flag=True,
+    help="wether to overrite the output folder if it exists",
 )
 def main(
     prompt_list,
     output_folder,
-    override_folder,
+    batch_size,
+    prompt_repeat,
     embed_text,
     tokenize_text,
     predict_image,
     clip_model,
     prior_checkpoint,
     prior_config,
+    override_folder,
 ):
     filename = filename_from_path(prompt_list)
     options = {
@@ -133,7 +209,7 @@ def main(
         click.secho(f"Dataset will be created at: {output_folder}", fg="magenta")
     except OSError:
         click.secho(
-            f"WARNING: The folder you specified already exists. This is a safety measure to avoid destroying existing work. If you wish to override it, please re-run this script with --override-folder=True.",
+            f"WARNING: The folder you specified already exists. This is a safety measure to avoid destroying existing work. If you wish to override it, please re-run this script with the --override-folder flag.",
             fg="red",
         )
 
@@ -142,7 +218,7 @@ def main(
     # load prompt list
 
     with open(prompt_list, "r") as f:
-        prompts = json.load(f)
+        prompts = np.array(json.load(f)).repeat(prompt_repeat)
 
     # grab models
 
@@ -162,11 +238,14 @@ def main(
 
     wds_create(
         prompts=prompts,
+        batch_size=batch_size,
         output_folder=output_folder,
         filename=filename,
         clip_model=clip_model,
         prior_model=prior_model,
-        options=options,
+        predict_image=predict_image,
+        embed_text=embed_text,
+        tokenize_text=tokenize_text,
     )
 
     click.secho(f"All done!", fg="green")
