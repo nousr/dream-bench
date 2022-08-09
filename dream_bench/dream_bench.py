31bee7345bcf611516c5f2be44e9729172cc485a
nousr <51308183+nousr@users.noreply.github.com>
2022-08-09 13:44:53 -0700

    cleanup main module

--- dream_bench/dream_bench.py
+++ dream_bench/dream_bench.py
@@ -1,21 +1,5 @@
-import torch
 import wandb
-from dream_bench.evaluator import Evaluator
 from dream_bench.config import DreamBenchConfig
-
-
-def evaluate(images: torch.Tensor, model_input: dict, evaluator: Evaluator):
-    """
-    Evaluate the predicted images from a model
-    """
-
-    # log the caption image pairs to wandb
-
-    captions = model_input["caption.txt"]
-
-    evaluator.add_pairs(captions=captions, images=images)
-
-    # TODO: other metrics computation
 
 
 def benchmark(adapter, config: DreamBenchConfig):
@@ -36,6 +20,3 @@ def benchmark(adapter, config: DreamBenchConfig):
         images = adapter(input_dict)
 
         evaluator.evaluate(model_input=input_dict, model_output=images)
-
-    # upload to wandb
-    evaluator.log_table()
