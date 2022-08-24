bc764b7b8f1c70013775d77a6df782f267539099
nousr <51308183+nousr@users.noreply.github.com>
2022-08-24 02:41:46 +0000

    remove extra line

--- dream_bench/dream_bench.py
+++ dream_bench/dream_bench.py
@@ -18,7 +18,6 @@ def benchmark(adapter, config: DreamBenchConfig):
     # begin benchmarking
     for input_dict in dataloader:
         images = adapter(input_dict)
-
         evaluator.evaluate(model_input=input_dict, model_output=images)
 
     evaluator.log()
